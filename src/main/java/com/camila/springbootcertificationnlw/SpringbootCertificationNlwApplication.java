a473b3f0cb65a7e3fc92b400bc091f4c4de77f92
Camila <alvescamila87@gmail.com>
2024-02-06 17:36:51 -0300

    create template spring iniciatilized

---
+++ src/main/java/com/camila/springbootcertificationnlw/SpringbootCertificationNlwApplication.java
@@ -0,0 +1,13 @@
+package com.camila.springbootcertificationnlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCertificationNlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCertificationNlwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camila/springbootcertificationnlw/SpringbootCertificationNlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camila.springbootcertificationnlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCertificationNlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
