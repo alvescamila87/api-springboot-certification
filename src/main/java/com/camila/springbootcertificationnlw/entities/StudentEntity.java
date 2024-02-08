f87bb198d037d1870cd963e6bf58d3f0619c1113
Camila <alvescamila87@gmail.com>
2024-02-08 19:35:59 -0300

    run docker

--- src/main/java/com/camila/springbootcertificationnlw/entities/StudentEntity.java
+++ src/main/java/com/camila/springbootcertificationnlw/entities/StudentEntity.java
@@ -1,5 +1,6 @@
 package com.camila.springbootcertificationnlw.entities;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
